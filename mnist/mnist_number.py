c6004a9e89064de934db2d87c94c1ff28cc6527b
daigouwei <daigouwei@gmail.com>
2018-01-31 21:55:10 +0800

    mnist增加模型测试

--- mnist/mnist_number.py
+++ mnist/mnist_number.py
@@ -24,3 +24,9 @@ sess.run(init)
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+
+
+# 评估模型
+correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+print(sess.run(accuracy, feed_dict={x:mnist.test.images, y_: mnist.test.labels}))
