01702021595a6444f60a8e5fdc08575e01339a13
daigouwei <daigouwei@gmail.com>
2018-03-06 17:50:27 +0800

    format

--- vocdata/create_imagesetsmain_4files.py
+++ vocdata/create_imagesetsmain_4files.py
@@ -9,18 +9,18 @@ total_xml = os.listdir(xmlfilepath)
 
 num = len(total_xml)
 list = range(num)
-tv = int(num*trainval_percent)
-tr = int(tv*train_percent)
+tv = int(num * trainval_percent)
+tr = int(tv * train_percent)
 trainval = random.sample(list, tv)
 train = random.sample(trainval, tr)
 
-ftrainval = open(txtsavepath+r'/trainval.txt', 'w')
-ftest = open(txtsavepath+r'/test.txt', 'w')
-ftrain = open(txtsavepath+r'/train.txt', 'w')
-fval = open(txtsavepath+r'/val.txt', 'w')
+ftrainval = open(txtsavepath + r'/trainval.txt', 'w')
+ftest = open(txtsavepath + r'/test.txt', 'w')
+ftrain = open(txtsavepath + r'/train.txt', 'w')
+fval = open(txtsavepath + r'/val.txt', 'w')
 
 for i in list:
-    name = total_xml[i][:-4]+'\n'
+    name = total_xml[i][:-4] + '\n'
     if i in trainval:
         ftrainval.write(name)
         if i in train:
--- vocdata/rename_voc_format.py
+++ vocdata/rename_voc_format.py
@@ -1,11 +1,13 @@
 import os
 
+
 class BatchRename():
     '''
     批量重命名文件夹中的图片文件名
     '''
+
     def __init__(self):
-        #我的图片文件夹路径
+        # 我的图片文件夹路径
         self.path = r'C:/Users/guowei/Desktop/train_model_myself/train'
 
     def rename(self):
@@ -15,9 +17,9 @@ class BatchRename():
         n = 6
         for item in filelist:
             if item.endswith('.jpg'):
-                n = 6-len(str(i))
+                n = 6 - len(str(i))
                 src = os.path.join(os.path.abspath(self.path), item)
-                dst = os.path.join(os.path.abspath(self.path), str(0)*n+str(i)+'.jpg')
+                dst = os.path.join(os.path.abspath(self.path), str(0) * n + str(i) + '.jpg')
                 try:
                     os.rename(src, dst)
                     print('converting {} to {} ...'.format(src, dst))
@@ -25,6 +27,7 @@ class BatchRename():
                 except:
                     continue
             print('total {} to rename and converted {} jpgs'.format(total_num, i))
+
 
 if __name__ == '__main__':
     app = BatchRename()
