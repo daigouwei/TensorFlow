580830897cd4af7a0bcb25a4a2e100321d7787ee
daigouwei <daigouwei@gmail.com>
2018-03-13 17:14:43 +0800

    理解mnist_cnn.py并增加必要的注释

--- kkeras/mnist_cnn.py
+++ kkeras/mnist_cnn.py
@@ -33,6 +33,7 @@ else:
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+# 归一化
 x_train /= 255
 x_test /= 255
 print('x_train shape:', x_train.shape)
@@ -40,6 +41,7 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
+# 标签转换为one-hot码
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
@@ -59,11 +61,13 @@ model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
+# verbose=1输出进度条记录
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           verbose=1,
           validation_data=(x_test, y_test))
+# verbose=0不输出日志信息
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
