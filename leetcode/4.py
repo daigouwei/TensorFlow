1a03c8bbee878b1be6e0f9fa27fa9cc62aaeb47b
daigouwei <daigouwei@gmail.com>
2018-05-13 18:54:32 +0800

    第四题有思路但是代码实现还是有问题

--- leetcode/4.py
+++ leetcode/4.py
@@ -1,3 +1,6 @@
+# 这题目只能理解思路，但是程序实现上感觉有点吃力,所以程序实现上还是有问题
+
+'''
 class MergeSort:
     def mergeArr(self, a, b):
         lenA = len(a)
@@ -16,8 +19,49 @@ class MergeSort:
         c.extend(a)
         c.extend(b)
         return c
+'''
+import sys
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # 必须保证对长度小的进行搜索，因为长度小的可能会越界
+        if len(nums1) > len(nums2):
+            tmp = nums1
+            nums1 = nums2
+            nums2 = tmp
+        leftIndex = 0
+        rightIndex = len(nums1) * 2
+        k = (len(nums1) + len(nums2)) // 2
+        while leftIndex <= rightIndex:
+            c1 = (leftIndex + rightIndex) // 2
+            c2 = k - c1
+            l1 = nums1[(c1 - 1) // 2] if c1 is not 0 else 0 - sys.maxsize
+            r1 = nums1[c1 // 2] if c1 is not len(nums1) * 2 else sys.maxsize
+            l2 = nums2[(c2 - 1) // 2] if c2 is not 0 else 0 - sys.maxsize
+            r2 = nums2[c2 // 2] if c2 is not len(nums2) * 2 else sys.maxsize
+            if l1 > r2:
+                rightIndex = c1
+            elif l2 > r1:
+                leftIndex = c1
+            else:
+                break
+        isOdd = self.checkOdd(len(nums1) + len(nums2))
+        if isOdd:
+            return max(r1, r2)
+        else:
+            return (max(l1, l2) + min(r1, r2)) / 2.0
+
+    def checkOdd(self, len):
+        if len % 2 == 1:
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
-    c = MergeSort().mergeArr([1, 2, 4], [2, 3, 5, 7])
-    print(c)
+    nums1 = [1, 2, 3, 4]
+    nums2 = [4, 5, 6, 7, 7, 7]
+    s = Solution()
+    hh = s.findMedianSortedArrays(nums1, nums2)
+    print(hh)
